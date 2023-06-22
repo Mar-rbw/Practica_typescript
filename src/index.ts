0508fc97284835abeed0907be9ec96650e2fe769
Robowa <marceli3@outlook.es>
2023-06-22 17:56:45 -0400

    varios
    Hubieron cambios varios

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,14 @@ mensaje = "chanchito feliz"
 console.log(mensaje)
 
 mensaje = "chao mundo"
+console.log(typeof []);
+let mama: string = "jaja"
+
+/* Tipos de datos
+number
+string
+boolean
+null
+undefined
+object //  arrays == object
+function */
