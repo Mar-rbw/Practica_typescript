df2ce00f66f0d7497340d51fb8400b4cb0864863
Robowa <marceli3@outlook.es>
2023-06-23 00:04:00 -0400

    agrego tema
    Se agrego el tema de las tuplas en TS

--- src/index.ts
+++ src/index.ts
@@ -41,12 +41,20 @@ function chanchitoFeliz(config:any){
     return config
 } */
 
-Tipos Array
+/* Tipos Array
 
 /* inferido */
-let oAnimales = ["chanchito","feliz","felipe"]
+/* let oAnimales = ["chanchito","feliz","felipe"] */
 /* No inferido */
-let eAnimales: string[] = ["chanchito","feliz","felipe"]
-let nums: number[] = [1, 2, 3]
-let checks: boolean[] = []
-let nums2: Array<number> = []
+/* let eAnimales: string[] = ["chanchito","feliz","felipe"] */
+
+/* let nums: number[] = [1, 2, 3]
+let checks: boolean[] = [] */
+/* arreglo de numbers */
+/* let nums2: Array<number> = [] */
+
+/* nums.map(x => x.) // el autocompletado sugiere métodos del tipo de dato */ 
+
+/* Tipos tupla
+ */
+let tupla: [number, string[]] = [1, ["chanchito feliz","chanchito felipe"]]
