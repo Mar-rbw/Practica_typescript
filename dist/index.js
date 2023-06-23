8de4e50c379068c7ef9bf32cc7dc1a45a9eb743a
Robowa <marceli3@outlook.es>
2023-06-22 23:12:50 -0400

    Temas varios
    Se escribio sobre los siguientes temas: tipos de datos en JS y TS, tipos de datos inferidos, tipo any y aplicado en funciones, se termino hablando del uso de arrays

--- dist/index.js
+++ dist/index.js
@@ -3,4 +3,6 @@ let mensaje = "Hola Mundo";
 mensaje = "chanchito feliz";
 console.log(mensaje);
 mensaje = "chao mundo";
+console.log(typeof []);
+let mama = "jaja";
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ mensaje = "chao mundo"
 console.log(typeof []);
 let mama: string = "jaja"
 
-/* Tipos de datos
+/* Tipos de datos => JS
 number
 string
 boolean
@@ -15,3 +15,38 @@ null
 undefined
 object //  arrays == object
 function */
+
+/* Tipos de datos => TS
+any
+unknown
+never
+Arrays
+tuplas
+Enums */
+
+/* Tipos inferidos
+
+let extincionDinosaurios: number = 76_000_000
+let dinosaurioFavorito: string = "T-rex"
+let extintos: boolean = true */
+
+/* Tipo de valor any
+let mivariable
+mivariable = "chanchito feliz"
+mivariable = 42 */
+
+/* Tipo de valor function
+
+function chanchitoFeliz(config:any){
+    return config
+} */
+
+Tipos Array
+
+/* inferido */
+let oAnimales = ["chanchito","feliz","felipe"]
+/* No inferido */
+let eAnimales: string[] = ["chanchito","feliz","felipe"]
+let nums: number[] = [1, 2, 3]
+let checks: boolean[] = []
+let nums2: Array<number> = []
